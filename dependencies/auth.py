b077575d72ddcd9c1086a71758055bc483692fda
evilsenpa1 <alexcorporationo@gmail.com>
2026-03-10 18:08:53 +0100

    Add deps for UserService class

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -1,8 +1,10 @@
 # auth/dependencies.py
 from fastapi import Depends
 from db.session import SessionDep 
-from repository.auth import UserRepository
+from repository.user import UserRepository
 from services.auth_service import AuthService
+from services.user_service import UserService
+from schemas.user_schema import UserPatchSchema
 from core.auth_security import security
 
 def get_user_repo(db: SessionDep) -> UserRepository:
@@ -13,6 +15,14 @@ def get_auth_service(
     repo: UserRepository = Depends(get_user_repo),
 ) -> AuthService:
     return AuthService(repo)
+
+
+def get_user_service(
+    repo: UserRepository = Depends(get_user_repo),
+) -> UserService:
+    return UserService(repo)
+
+
 
 def get_current_user_id(payload=Depends(security.access_token_required)) -> int:
     return int(payload.sub)
