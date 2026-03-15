a36d4a7aac070c557a6a4bee23e5ad0cfa18b806
evilsenpa1 <alexcorporationo@gmail.com>
2026-03-15 17:06:10 +0100

    Added staff check for couple of routes

--- api/v1/routers/user_router.py
+++ api/v1/routers/user_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from services.user_service import UserService
-from dependencies.auth_dep import get_user_service
+from dependencies.auth_dep import get_user_service, require_staff
 from schemas.user_schema import UserPatchSchema
 from core.auth_security import security
 from dependencies.auth_dep import get_current_user_id
@@ -8,13 +8,13 @@ from dependencies.auth_dep import get_current_user_id
 router = APIRouter()
 
 
-@router.get("/all_users", dependencies=[Depends(security.access_token_required)])
+@router.get("/all_users", dependencies=[Depends(security.access_token_required), Depends(require_staff)])
 async def all_users_route(service: UserService = Depends(get_user_service)):
     return await service.all_users()
 
 
 @router.get(
-    "/user_by_id/{user_id}", dependencies=[Depends(security.access_token_required)]
+    "/user_by_id/{user_id}", dependencies=[Depends(security.access_token_required), Depends(require_staff)]
 )
 async def user_by_id_route(
     user_id: int, service: UserService = Depends(get_user_service)
