b0d9e17cd0b74ea02e709207f80f4c5e0124a005
evilsenpa1 <alexcorporationo@gmail.com>
2026-03-15 17:07:29 +0100

    Minot chamges in deps + base dep for get repository and service (book)

--- dependencies/author_dep.py
+++ dependencies/author_dep.py
@@ -1,15 +1,15 @@
 from fastapi import Depends
 from db.session import SessionDep
 from repository.author_repo import AuthorRepository
-from services.auth_service import AuthService
+from services.author_service import AuthorService
 from core.auth_security import security
 
 
 def get_author_repo(db: SessionDep) -> AuthorRepository:
-    return AuthorRepository(db)
+    return AuthorRepository(db=db)
 
 
 def get_author_service(
     repo: AuthorRepository = Depends(get_author_repo),
-) -> AuthService:
-    return AuthService(repo)
+) -> AuthorService:
+    return AuthorService(repo)
