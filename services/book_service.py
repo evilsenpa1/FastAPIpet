201a7d09cd444bf278e7cb163c971c02049a46cb
evilsenpa1 <alexcorporationo@gmail.com>
2026-03-15 17:12:43 +0100

    New service (now with class)

--- services/book_service.py
+++ services/book_service.py
@@ -1,14 +1,13 @@
 from schemas.book_schema import (
     BookAddSchema,
-    AuthorAddSchema,
     BookDeleteSchema,
-    BookUpdateSchema,
-    AuthorUpdateSchema,
-    AuthorDeleteSchema,
+    BookPatchSchema,
 )
 from db.session import SessionDep
 from models.book_model import BookModel, AuthorModel
 from repository.book_repo import BookRepository
+from repository.author_repo import AuthorRepository
+from dependencies.author_dep import get_author_repo
 from sqlalchemy import select
 from fastapi import HTTPException, File, UploadFile, Depends
 import aiofiles
@@ -17,133 +16,85 @@ import uuid
 from utils import file_manager
 
 
-async def get_books(session: SessionDep):
-    query = select(BookModel)
-    result = await session.execute(query)
-    return result.scalars().all()
+class BookService:
+    def __init__(self, repo: BookRepository):
+        self.repo = repo
+
+    async def get_all_books(self):
+        return await self.repo.get_all()
+
+    async def get_book_by_id(self, id: int):
+        return await self.repo.get_by_id(id)
+
+    async def create_book(self, user_id: int, file, data: BookAddSchema, author_repo):
+
+        await self._validate_authors(data.authors_ids)
+
+        file_path = await self._get_file_path(user_id, file)
+
+        await file_manager.create_file(file_path, file)
+
+        file_path = str(file_path)
+
+        data.file_path = file_path
+
+        try:
+            authors_ids = data.authors_ids
+            book_data = data.model_dump(exclude={"authors_ids"})
+            new_book = await self.repo.create(book_data, authors_ids, author_repo)
+        except HTTPException:
+            file_manager.delete_file(file_path)
+            raise
+        except Exception as e:
+            file_manager.delete_file(file_path)
+            print(f"ERROR: {e}", flush=True)
+            import traceback
+
+            traceback.print_exc()
+            raise HTTPException(status_code=500, detail=f"Internal server error")
+
+        return new_book
+
+    async def patch_book(self, id: int, data: BookPatchSchema) -> BookModel:
+        if data.authors_ids is not None:
+            await self._validate_authors(data.authors_ids)
+
+        return await self.repo.patch(id, data)
+
+    async def delete_book(self, id: int):
+        return await self.repo.delete(id)
+
+    async def _validate_authors(
+        self,
+        author_ids: list[int],
+    ) -> None:
+        author_repo = get_author_repo(self.repo.db)
+        authors = await author_repo.get_list_by_ids(author_ids)
+        if len(authors) != len(author_ids):
+            raise HTTPException(status_code=404, detail="One or more authors not found")
+
+    async def _get_file_path(self, user_id: int, file: str) -> Path:
+
+        if not file:
+            return {"message": "No upload file sent"}
+
+        UPLOAD_DIR = Path("uploads")
+        UPLOAD_DIR.mkdir(exist_ok=True)
+        ALLOWED_EXTENSIONS = {".doc", ".docx", ".pdf", ".txt"}
+
+        file_ext = Path(file.filename).suffix.lower()
+
+        if file_ext not in ALLOWED_EXTENSIONS:
+            raise HTTPException(
+                status_code=400,
+                detail=f"File type '{file_ext}' not allowed. Use: {ALLOWED_EXTENSIONS}",
+            )
+
+        unique_name = f"{uuid.uuid4()}{file_ext}"
+
+        user_dir = UPLOAD_DIR / str(user_id)
+        user_dir.mkdir(parents=True, exist_ok=True)
+
+        return user_dir / unique_name
 
 
-async def get_authors(session: SessionDep):
-    query = select(AuthorModel)
-    result = await session.execute(query)
-    return result.scalars().all()
-
-
-
-
-
-async def update_book(data: BookUpdateSchema, session: SessionDep):
-
-    result = await session.execute(select(BookModel).filter(BookModel.id == data.id))
-    book = result.scalars().first()
-
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-
-    book.name = "New name"
-    book.year = 6666
-
-    await session.commit()
-    return {"status": "success"}
-
-
-async def delete_book(data: BookDeleteSchema, session: SessionDep):
-
-    result = await session.execute(select(BookModel).filter(BookModel.id == data.id))
-    book = result.scalars().first()
-
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-
-    await session.delete(book)
-    await session.commit()
-    return {"status": "success"}
-
-
-async def add_author(data: AuthorAddSchema, session: SessionDep):
-
-    new_author = AuthorModel(
-        author=data.author,
-    )
-    session.add(new_author)
-    await session.commit()
-    return {"status": "success"}
-
-
-async def update_author(data: AuthorUpdateSchema, session: SessionDep):
-
-    result = await session.execute(
-        select(AuthorModel).filter(AuthorModel.id == data.id)
-    )
-    author = result.scalars().first()
-
-    if not author:
-        raise HTTPException(status_code=404, detail="Author not found")
-
-    author.name = "New author"
-
-    await session.commit()
-    return {"status": "success"}
-
-
-async def delete_author(data: AuthorDeleteSchema, session: SessionDep):
-
-    result = await session.execute(
-        select(AuthorModel).filter(AuthorModel.id == data.id)
-    )
-    author = result.scalars().first()
-
-    if not author:
-        raise HTTPException(status_code=404, detail="Book not found")
-
-    await session.delete(author)
-    await session.commit()
-    return {"status": "success"}
-
-
-UPLOAD_DIR = Path("uploads")
-UPLOAD_DIR.mkdir(exist_ok=True)
-ALLOWED_EXTENSIONS = {".doc", ".docx", ".pdf", ".txt"}
-
-
-def get_file_path(user_id: int, filename: str) -> Path:
-    file_ext = Path(filename).suffix.lower()
-    unique_name = f"{uuid.uuid4()}{file_ext}"
-
-    user_dir = UPLOAD_DIR / str(user_id)
-    user_dir.mkdir(parents=True, exist_ok=True)
-
-    return user_dir / unique_name
-
-
-async def upload_book(user_id: int, file: UploadFile, data: BookAddSchema, repo: BookRepository):
-    if not file:
-        return {"message": "No upload file sent"}
-
-    file_ext = Path(file.filename).suffix.lower()
-
-    if file_ext not in ALLOWED_EXTENSIONS:
-        raise HTTPException(
-            status_code=400,
-            detail=f"File type '{file_ext}' not allowed. Use: {ALLOWED_EXTENSIONS}",
-        )
-
-    file_path = get_file_path(user_id, file.filename)
-
-    await file_manager.create_file(file_path, file)
-
-
-    try:
-        await repo.create_book(file_path, data)
-    except HTTPException:
-        file_manager.delete_file(file_path)
-        raise
-    except Exception as e:
-        file_manager.delete_file(file_path)
-        print(f"ERROR: {e}", flush=True)
-        import traceback
-
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail=f"Internal server error")
-
-    return {"filename": file.filename}
