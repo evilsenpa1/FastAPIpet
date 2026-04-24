e2704de7ee79fe1f475cf988565944704034163a
evilsenpa1 <alexcorporationo@gmail.com>
2026-04-24 23:47:27 +0200

    refactor: Delete non-usable imports and env variables

--- db/base.py
+++ db/base.py
@@ -1,5 +1,3 @@
-import os
-from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 
 from core.settings import DATABASE_URL
@@ -7,17 +5,7 @@ from core.settings import DATABASE_URL
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-load_dotenv()
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-SQLALCHEMY_DATABASE_URL = DATABASE_URL
-
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_async_engine(DATABASE_URL)
 
 SessionLocal = async_sessionmaker(autocommit = False, autoflush=False, expire_on_commit=False, bind=engine)
 
