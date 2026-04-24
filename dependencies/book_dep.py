183c4c9325b62fcbe54904ae47f7186a26460192
evilsenpa1 <alexcorporationo@gmail.com>
2026-04-24 23:58:41 +0200

    feat: add patch router for books, rename some routes

--- dependencies/book_dep.py
+++ dependencies/book_dep.py
@@ -2,7 +2,7 @@ from db.session import SessionDep
 from repository.book_repo import BookRepository
 from services.book_service import BookService
 from fastapi import HTTPException, Form, Depends
-from schemas.book_schema import BookAddSchema
+from schemas.book_schema import BookCreateRequest
 import json
 
 
@@ -17,8 +17,8 @@ def get_book_service(
     return BookService(repo)
 
 
-def parse_book_data(data: str = Form(...)) -> BookAddSchema:
+def parse_book_data(data: str = Form(...)) -> BookCreateRequest:
     try:
-        return BookAddSchema(**json.loads(data))
+        return BookCreateRequest(**json.loads(data))
     except (json.JSONDecodeError, ValueError) as e:
         raise HTTPException(status_code=422, detail=f"Invalid book data: {e}")
