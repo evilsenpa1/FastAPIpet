1e2c72b16dcebc0df0fef57e35de5805926cc257
evilsenpa1 <alexcorporationo@gmail.com>
2026-04-24 23:44:42 +0200

    refactor: Delete non-usable imports

--- repository/user_repo.py
+++ repository/user_repo.py
@@ -2,7 +2,6 @@ from fastapi import HTTPException
 from sqlalchemy import select
 from db.session import SessionDep
 from models.auth_model import UserModel
-from schemas.user_schema import UserPatchSchema
 from utils.crud import CrudBase
 
 
